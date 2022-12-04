7a01d5adf0f8ec60748edf76d73be9474a19db4a
desantamaria <santamaria.daniel316@gmail.com>
2022-12-03 19:22:34 -0800

    game now accepts user input

--- FinalgroupProject.asm
+++ FinalgroupProject.asm
@@ -25,6 +25,8 @@
 		li $v0, 1
 		move $a0, $s0
 		syscall
+		
+		aString("  ")
 		#increase $t0 by 3, the size of a word (so we can go to next word in array)
 		addi $t0, $t0, 4
 		
@@ -46,6 +48,22 @@
 	syscall
 .end_macro
 
+# Macro for printing strings declared in .data
+.macro printBoard
+	aString("   c1 c2 c3\n")
+	
+	aString("r1 ")
+	Parray(row1)
+	aString("\n")
+	aString("r2 ")
+	Parray(row2)
+	aString("\n")
+	aString("r3 ")
+	Parray(row3)
+	aString("\n")
+	
+.end_macro
+
 .data
 
 	row1: .word 0, 0, 0
@@ -53,7 +71,7 @@
 	row3:.word 0, 0, 0 
 
 	Table: .word row1, row2, row3
-	message: .asciiz "\nPlayer 1: "
+	message: .asciiz "\nPlayer 1's Turn \n"
 	space: .asciiz " "
 .text
 
@@ -61,9 +79,109 @@ main:
 	printString(message)
 	#create a macro for the table
 	
+	aString("   c1 c2 c3\n")
+	
+	aString("r1 ")
 	Parray(row1)
 	aString("\n")
+	aString("r2 ")
 	Parray(row2)
 	aString("\n")
+	aString("r3 ")
 	Parray(row3)
 	aString("\n")
+	
+	j player1
+
+
+player1:
+	
+	aString("\nEnter a row ")
+	li $v0, 5
+	syscall	
+	move $s0, $v0
+	
+	aString("\nEnter a column ")
+	li $v0, 5
+	syscall	
+	move $s1, $v0
+	
+	beq $s0, 1, r1
+	beq $s0, 2, r2
+	beq $s0, 3, r3
+	
+	
+r1:
+	beq $s1, 1, row1col1
+	beq $s1, 2, row1col2
+	beq $s1, 3, row1col3
+
+r2:
+	beq $s1, 1, row2col1
+	beq $s1, 2, row2col2
+	beq $s1, 3, row2col3
+	
+r3:
+	beq $s1, 1, row3col1
+	beq $s1, 2, row3col2
+	beq $s1, 3, row3col3	
+
+row1col1:
+	la $t0, row1
+	li $t1, 1
+	sw $t1, 0($t0)
+	
+	printBoard
+	
+	
+row1col2:
+	la $t0, row1
+	li $t1, 1
+	sw $t1, 4($t0)
+	
+	printBoard	
+		
+row1col3:
+	la $t0, row1
+	li $t1, 1
+	sw $t1, 8($t0)
+	
+	printBoard
+row2col1:
+	la $t0, row2
+	li $t1, 1
+	sw $t1, 0($t0)
+	
+	printBoard
+row2col2:
+	la $t0, row2
+	li $t1, 1
+	sw $t1, 4($t0)
+	
+	printBoard
+row2col3:
+	la $t0, row2
+	li $t1, 1
+	sw $t1, 8($t0)
+	
+	printBoard
+row3col1:
+	la $t0, row3
+	li $t1, 1
+	sw $t1, 0($t0)
+	
+	printBoard
+row3col2:
+	la $t0, row3
+	li $t1, 1
+	sw $t1, 4($t0)
+	
+	printBoard
+row3col3:
+	la $t0, row3
+	li $t1, 1
+	sw $t1, 8($t0)
+	
+	printBoard
+
+exit: exit
